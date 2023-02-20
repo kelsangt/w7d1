63efd921d2d01b5f5cf5a54e2587dc9c78e4b620
Jordan Buranskas <jordanburanskas@gmail.com>
2023-02-20 15:25:13 -0500

    first p

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -3,11 +3,36 @@ class User < ApplicationRecord
     validates :password_digest, presence: true
     before_validations :ensure_session_token 
 
+    def find_by_credentials(username, password)
+        user_inst = User.find_by(username: username)
+        if user_inst && user_inst.correct_password?(password)
+             @user_inst
+        else
+            nil
+        end
+
+    end
+
     attr_reader :password 
+
 
     def password=(password)
         self.password_digest = Bcrypt::Password.create(password)
         @password = password
+    end
+
+
+    def correct_password?(password)
+        bcypt_obj = Bcrypt::Password.new(self.password_digest)
+        bcypt_obj.is_password?(password)
+    end
+
+
+    def reset_session_token!
+        Self.session_token = generate_unique_session_token 
+        Self.save!
+        Self.session_token
+
     end
 
     private 
